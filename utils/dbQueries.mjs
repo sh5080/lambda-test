4ec789b10adcf4d2ced4751f069381152b154458
sh5080 <prpn97@gmail.com>
2023-12-04 14:55:27 +0900

    feat: 미선택, 자동휴면 쿼리추가

--- utils/dbQueries.mjs
+++ utils/dbQueries.mjs
@@ -1,3 +1,4 @@
+// deadline 기간 내 수락
 export const getAccessDataQuery = () => {
   return `
       SELECT mp.female_id, mp.male_id, 
@@ -12,7 +13,7 @@ export const getAccessDataQuery = () => {
         AND mp.m_choice = 1;
     `;
 };
-
+// deadline 기간 내 거절
 export const getRejectDataQuery = () => {
   return `
         SELECT mp.female_id, mp.male_id, 
@@ -25,5 +26,36 @@ export const getRejectDataQuery = () => {
           AND mp.deadline > NOW()
           AND mp.f_choice = -1
           AND mp.m_choice = -1;
+      `;
+};
+// deadline 기간 내 미선택
+export const getNonSelectDataQuery = () => {
+  return `
+        SELECT mp.female_id, mp.male_id, 
+        male.mobile_number AS male_number, female.mobile_number AS female_number
+        FROM matching_public mp
+        JOIN global_status gs ON mp.phase = gs.phase
+        LEFT JOIN users female ON mp.female_id = female.id
+        LEFT JOIN users male ON mp.male_id = male.id
+        WHERE gs.id = (SELECT MAX(id) FROM global_status)
+          AND mp.deadline > NOW()
+          AND mp.f_choice = 0
+          AND mp.m_choice = 0;
+      `;
+};
+
+// deadline 기간 지나고 권유 문자 받고도 아직도 choice = 0
+export const getDormantDataQuery = () => {
+  return `
+        SELECT mp.female_id, mp.male_id, 
+        male.mobile_number AS male_number, female.mobile_number AS female_number
+        FROM matching_public mp
+        JOIN global_status gs ON mp.phase = gs.phase
+        LEFT JOIN users female ON mp.female_id = female.id
+        LEFT JOIN users male ON mp.male_id = male.id
+        WHERE gs.id = (SELECT MAX(id) FROM global_status)
+          AND mp.deadline < NOW()
+          AND mp.f_choice = 0
+          AND mp.m_choice = 0;
       `;
 };
